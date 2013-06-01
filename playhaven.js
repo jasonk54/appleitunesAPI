90cd7a94ecfcdc60ac7bdf4d9a9dcd5fe5a500ec
Jason K <jasonk54@gmail.com>
2013-05-31 17:56:01 -0700

    refactored js code

--- playhaven.js
+++ playhaven.js
@@ -1,44 +1,48 @@
-$(document).ready(function(){
-  var timer;
+var PlayHaven = function() {
+  var timer, text;
   var typingInterval = 3000;
-
-  $('#searchBox').keyup(function() {
-    // Replace this with underscore's debounce method
-    clearTimeout(timer);
-    if ($('#searchBox').val()) {
-      timer = setTimeout(doneTyping, typingInterval);
-    }
-  });
-
-  var doneTyping = function() {
-    var text = $('#searchBox').val();
-    var appleAJAXHelpers = {
-      success: function(data, status, jqXHR) {
-        console.log(data);
-        parser(data, 'artist');
-      },
-      error: function(jqXHR, status, error) {
-        console.log(error);
-      }
-    }
-
-    var parser = function(data, option) {
+  var helpers = {
+    doneTyping: function() {
+      text = $('#searchBox').val();
+      $.ajax({
+        url: "https://itunes.apple.com/search",
+        data: {
+          term: "jason"
+        },
+        jsonp: 'callback',
+        dataType: "jsonp",
+        success: appleAJAXHandlers.success,
+        error: appleAJAXHandlers.error
+      });
+    },
+    parser: function(data) {
       for (var i = 0; i < data.results.length; i++) {
         // replace this with a templating engine, such as handlerbars or underscore
         $('.searchContent').append("<div>" + data.results[i].artistName + "</div>");
       }
     }
+  };
 
-    // comments please!
-    $.ajax({
-      url: "https://itunes.apple.com/search",
-      data: {
-        term: text
-      },
-      jsonp: 'callback',
-      dataType: "jsonp",
-      success: appleAJAXHelpers.success,
-      error: appleAJAXHelpers.error
-    });
-  }
+  var appleAJAXHandlers = {
+    success: function(data, status, jqXHR) {
+      // debugger;
+      helpers.parser(data);
+    },
+    error: function(jqXHR, status, error) {
+      console.log(error);
+    }
+  };
+
+  // comments please!
+  $('#searchBox').keyup(function() {
+    // Replace this with underscore's debounce method
+    clearTimeout(timer);
+    if ($('#searchBox').val()) {
+      timer = setTimeout(helpers.doneTyping, typingInterval);
+    }
+  });
+};
+
+$(document).ready(function(){
+  PlayHaven();
 });
