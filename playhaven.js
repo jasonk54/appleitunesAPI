660c1ac0637045c48a20ada03d7a4e275cfb6eb0
Jason K <jasonk54@gmail.com>
2013-05-31 17:28:38 -0700

    Gathers and appends data from itunes.

--- playhaven.js
+++ playhaven.js
@@ -1,22 +1,44 @@
 $(document).ready(function(){
-  $('#searchBox').keyup(function(){
-      var search = $('#searchBox').val();
-      var appleAJAXHelpers = {
-        success: function(data, status, jqXHR) {
-        $("body").append(data.results[0].artistName);
+  var timer;
+  var typingInterval = 3000;
+
+  $('#searchBox').keyup(function() {
+    // Replace this with underscore's debounce method
+    clearTimeout(timer);
+    if ($('#searchBox').val()) {
+      timer = setTimeout(doneTyping, typingInterval);
+    }
+  });
+
+  var doneTyping = function() {
+    var text = $('#searchBox').val();
+    var appleAJAXHelpers = {
+      success: function(data, status, jqXHR) {
+        console.log(data);
+        parser(data, 'artist');
       },
       error: function(jqXHR, status, error) {
+        console.log(error);
       }
-    };
+    }
+
+    var parser = function(data, option) {
+      for (var i = 0; i < data.results.length; i++) {
+        // replace this with a templating engine, such as handlerbars or underscore
+        $('.searchContent').append("<div>" + data.results[i].artistName + "</div>");
+      }
+    }
+
+    // comments please!
     $.ajax({
       url: "https://itunes.apple.com/search",
       data: {
-        term: search
+        term: text
       },
       jsonp: 'callback',
       dataType: "jsonp",
       success: appleAJAXHelpers.success,
       error: appleAJAXHelpers.error
     });
-  });
+  }
 });
