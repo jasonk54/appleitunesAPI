ae53c326e3324b2f517e7a343824ec1103f011cb
Jason K <jasonk54@gmail.com>
2013-11-07 15:00:38 -0800

    refactor

--- apple.js
+++ apple.js
@@ -1,14 +1,13 @@
-// TODO: Add templating
-// TODO: Use backbone
 
 var apple = function() {
   var timer, text, entity;
   var typingInterval = 500;
+  var current_request = null;
 
   var helpers = {
-    doneTyping: function() {
 
-      text = $('#searchBox').val();
+    // AJAX request to fetch data
+    doneTyping: function(text) {
       return $.ajax({
         url: "https://itunes.apple.com/search",
         data: {
@@ -16,74 +15,75 @@ var apple = function() {
         },
         jsonp: 'callback',
         dataType: "jsonp",
-        success: appleAJAXHandlers.success,
-        error: appleAJAXHandlers.error
+        success: function(resp) {
+          helpers.parsedOutput(resp);
+          current_request = null;
+        },
+        error: function() {
+          current_request = null;
+        }
       });
     },
 
-    // Parses data, and popluates the correct content
+    // Parses the data, and popluates the correct content
     parsedOutput: function(data) {
       // Removes duplicate copy of the entities property and output type of files
       var getKind = _.pluck(data.results, 'kind');
       var kind = _.uniq(getKind);
+
       $('.greetings').hide('slow');
       $('.side').show();
       $('.content').show();
 
-      // Appends type of files
-      $('.sidePanel').empty();                           // Clears before appending
-      for (var i = 0; i < kind.length; i++) {
-         $('.sidePanel').append("<div class='type'>" + kind[i] +  "</div>");
-      };
-
-      // Appends artist name and track name.  All genre
-      $('.searchContent').empty();                      // Clears before appending
-      for (var i = 0; i < data.results.length; i++) {
-        $('.searchContent').append("<div class='contentlist'>Name: " + data.results[i].artistName + "</div>");
-        $('.searchContent').append("<div class='contentlist'>Track Name: " + data.results[i].trackName + "</div>");
-        $('.searchContent').append("<div class='contentlist'>File Type: " + data.results[i].kind + "</div>");
-        $('.searchContent').append("<input type='button' class='preview btn btn-primary' data-previewfile='" + data.results[i].previewUrl + "' value='Preview'></input>");
-      };
+      this.handlebarCompiler(kind, "#type-of-content", ".side");
+      this.handlebarCompiler(data.results, "#content-template", ".searchContent")
 
       // Embeds media file on the DOM - via Modal - when preview button is clicked.
       $('.preview').click(function(el) {
         var previewLink = $(el.target).data('previewfile');
         if (previewLink) {
-          $('.modal-body').empty().append("<div><embed src=" + previewLink + " width='100%' height='100%'></div>");
+          $('.modal-body').html("<div><embed src=" + previewLink + " width='100%' height='100%'></div>");
           $('#myModal').modal('show');
         } else {
           alert('Preview not available');
         }
       });
-    }
-  };
-
-  var current_xhr = null;
-
-  var appleAJAXHandlers = {
-    success: function(data, status, jqXHR) {
-      helpers.parsedOutput(data);
-      current_xhr = null;
     },
-    error: function(jqXHR, status, error) {
-      console.log(error);
-      current_xhr = null;
+
+    // Handlebars compile method
+    handlebarCompiler : function(data, templateScript, target) {
+      var source   = $(templateScript).html();
+      var template = Handlebars.compile(source);
+      $(target).html(template({content : data}));
     }
   };
 
   // Call helpers.doneTyping on last keystroke (on release) after .5 seconds
   $('#searchBox').keyup(function() {
+    var text = $('#searchBox').val();
     clearTimeout(timer);
-    if ($('#searchBox').val()) {
+    if (text) {
       timer = setTimeout(function() {
-        if (current_xhr) current_xhr.abort();
-        current_xhr = helpers.doneTyping();
+        if (current_request) current_request.abort();
+        current_request = helpers.doneTyping(text);
       }, typingInterval);
     }
   });
 };
 
 $(document).ready(function(){
+  
+  // Handlebars helper
+  Handlebars.registerHelper('typeOfFile', function(type) {
+    if (type === "song" || type === "podcast") {
+      return new Handlebars.SafeString('<i class="fa fa-music"></i>');
+    } else if (type === "feature-movie" || type === "tv-episode") {
+      return new Handlebars.SafeString('<i class="fa fa-film"></i>');
+    } else {
+      return type; 
+    }
+  });
+
   apple();
   $('input').speechToText();
 });
